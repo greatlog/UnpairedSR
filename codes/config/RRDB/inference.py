d80597992b03ea06b850abc0cb22b76d00fa8b28
log <lzxlog@qq.com>
2021-06-23 17:02:58 +0800

    update

--- codes/config/RRDB/inference.py
+++ codes/config/RRDB/inference.py
@@ -5,10 +5,7 @@ import os
 import os.path as osp
 import random
 import sys
-import cv2
 from collections import defaultdict
-from glob import glob
-from tqdm import tqdm
 
 import numpy as np
 import torch
@@ -28,13 +25,13 @@ from models import create_model
 #### options
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--opt",
+    "-opt",
     type=str,
     default="options/setting1/test/test_setting1_x4.yml",
     help="Path to options YMAL file.",
 )
-parser.add_argument("--input_dir", type=str, default="/mnt/hdd/lzx/SRDatasets/NTIRE2020/track1/track1_test_input/")
-parser.add_argument("--output_dir", type=str, default="results/2020Track1Test/")
+parser.add_argument("-input_dir", type=str, default="../../../data_samples/LR")
+parser.add_argument("-output_dir", type=str, default="../../../data_samples/DANv1_SR")
 args = parser.parse_args()
 opt = option.parse(args.opt, is_train=False)
 
@@ -54,10 +51,9 @@ for inx, path in tqdm(enumerate(test_files)):
     img_t = torch.as_tensor(np.ascontiguousarray(img)).float()
 
     model.test(img_t)
-    visuals = model.get_current_visuals()
 
-    sr_im = util.tensor2img(visuals["sr"])
-
+    sr = model.fake_SR.detach().float().cpu()[0]
+    sr_im = util.tensor2img(sr)
 
     save_path = osp.join(args.output_dir, "{}_x{}.png".format(name, opt["scale"]))
     cv2.imwrite(save_path, sr_im)
